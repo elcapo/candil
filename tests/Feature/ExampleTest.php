9e9a3ececbda4df953e52e46bc0f587802b38625
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2023-08-21 10:54:26 +0100

    install pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,19 +1,7 @@
 <?php
 
-namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+use function Pest\Livewire\livewire;
+ 
+it('can run feature tests', function () {
+    livewire('NonExisting::class');
+})->throws(\Exception::class);
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,5 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+it('can run unit tests', function () {
+    expect(1+1)->toBe(2);
+});
